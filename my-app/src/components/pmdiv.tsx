bd0bdcf228f9b38e0e95c82a93f0a3fe553fcc76
unknown <allanjiogamer@gmail.com>
2025-06-21 08:52:32 +0530

    added pmdiv modifications

--- my-app/src/components/pmdiv.tsx
+++ my-app/src/components/pmdiv.tsx
@@ -1,17 +1,19 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCircleChevronRight, faPaperPlane, faSmile } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faCircleChevronRight, faPaperPlane, faPencil, faSmile, faTrash } from "@fortawesome/free-solid-svg-icons";
 import Avatar from "./avatar";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { faChevronCircleLeft } from "@fortawesome/free-solid-svg-icons/faChevronCircleLeft";
 
 type Privatelogsprop = {
     message: string;
     userid: number;
+    time: string;
 }
 
 function Pmdiv() {
     const [isdivselected, setisdivselected] = useState<boolean>(false);
     const [selectedusername, setselectedusername] = useState<string>("");
+
     return (
         <div className="w-full h-full bg-amber-50">
             {isdivselected && <Pmusercontainer setisdivselected={setisdivselected} selectedusername={selectedusername} />}
@@ -38,6 +40,14 @@ function Pmuserdiv({ setisdivselected, setselectedusername }: any) {
 }
 
 function Pmusercontainer({ setisdivselected, selectedusername }: any) {
+    const pmcontainerref = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (pmcontainerref.current) {
+            pmcontainerref.current.scrollTop = pmcontainerref.current.scrollHeight;
+        }
+    }, []);
+
     return (
         <div className="w-full h-full flex flex-col">
             <div className="w-full h-[50px] bg-black flex items-center " onClick={() => setisdivselected(false)}>
@@ -47,9 +57,9 @@ function Pmusercontainer({ setisdivselected, selectedusername }: any) {
                 </div>
                 <span className="text-white px-2">{selectedusername}</span>
             </div>
-            <div className="w-full flex-grow bg-green-500 flex flex-col overflow-y-scroll">
-                <Privatelogs message="allah" userid={191} />
-                <Privatelogs message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sodales eleifend tellus quis congue. Quisque rutrum nisi dolor, in scelerisque est ullamcorper et. Curabitur in aliquam elit, non ultrices diam. Proin a facilisis magna. Duis molestie massa ut turpis ultrices pulvinar. Etiam eget neque eleifend, egestas felis a, tincidunt felis. Curabitur mollis vehicula rhoncus. Nam tincidunt odio quis tellus posuere tincidunt. Donec interdum arcu in est feugiat, sed elementum libero dictum. Quisque eget turpis pharetra arcu vulputate ultrices." userid={192} />
+            <div className="w-full flex-grow bg-green-500 flex flex-col overflow-y-scroll" ref={pmcontainerref}>
+                <Privatelogs message="allah" userid={191} time="12:00pm" />
+                <Privatelogs message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sodales eleifend tellus quis congue. Quisque rutrum nisi dolor, in scelerisque est ullamcorper et. Curabitur in aliquam elit, non ultrices diam. Proin a facilisis magna. Duis molestie massa ut turpis ultrices pulvinar. Etiam eget neque eleifend, egestas felis a, tincidunt felis. Curabitur mollis vehicula rhoncus. Nam tincidunt odio quis tellus posuere tincidunt. Donec interdum arcu in est feugiat, sed elementum libero dictum. Quisque eget turpis pharetra arcu vulputate ultrices." userid={192} time="12:00pm" />
             </div>
             <div className="w-full h-[50px] bg-black px-2">
                 <TextBar />
@@ -58,12 +68,34 @@ function Pmusercontainer({ setisdivselected, selectedusername }: any) {
     )
 }
 
-function Privatelogs({ message, userid }: Privatelogsprop) {
+function Privatelogs({ message, userid, time }: Privatelogsprop) {
+    const [privatelogoptions, setprivatelogsoptions] = useState<boolean>(true);
     return (
-        <div className={`max-w-[80%] h-fit p-2 my-2 bg-black text-white ${userid == 191 ? 'ms-auto' : 'me-auto'}`}>{message}</div>
+        <div className={`max-w-[80%] h-fit p-2 my-2 bg-black text-white flex flex-row ${userid == 191 ? 'ms-auto' : 'me-auto'}`} onClick={() => alert("ok")}>
+
+            <div className="flex flex-col">
+                <div>
+                    {message}
+                </div>
+                <div>
+                    {time}
+                </div>
+            </div>
+            {privatelogoptions && (
+                <Privatelogsoptions />
+            )}
+        </div>
     )
 }
 
+function Privatelogsoptions() {
+    return (
+        <div className="flex flex-col items-center justify-evenly border-l-2 border-blue-500">
+            <FontAwesomeIcon icon={faTrash} />
+            <FontAwesomeIcon icon={faPencil} />
+        </div>
+    )
+}
 function TextBar() {
     const [searchtext, setsearchtext] = useState("");
     return (
